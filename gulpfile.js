db94f5d28f4f5bd020e08ecc0fa6209e25ee1950
Chad Engler <englercj@live.com>
2014-10-04 12:44:49 -0700

    add dev task

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     tiledmapPack = require('./node_modules/phaser-tiled/tools/gulp-tiled-pack');
 
-gulp.task('connect', function() {
-    connect.server();
-});
-
 gulp.task('pack', function () {
     return gulp.src('./maps/**/*.{json,tmx}')
         .pipe(tiledmapPack({ useExtInKey: true, baseUrl: 'maps' }))
         .pipe(gulp.dest('./maps'));
 });
 
-gulp.task('default', ['pack', 'connect']);
+gulp.task('dev', ['pack'], function() {
+    connect.server();
+});
+
+gulp.task('default', ['pack']);
