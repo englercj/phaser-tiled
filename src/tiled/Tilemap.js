dfdcf73fa795e9142cdef80ba4d32dcfa57ca6f6
Silvan Strübi <silvan.struebi@gmail.com>
2016-01-21 14:17:39 +0900

    Addition: if x/y else offsetx/offsety
    tiled does sometimes use x/y and sometimes offsetx/offsety

--- src/tiled/Tilemap.js
+++ src/tiled/Tilemap.js
@@ -167,7 +167,9 @@ function Tilemap(game, key, group) {
                 break;
 
             case 'imagelayer':
-                lyr = game.add.sprite(ldata.x, ldata.y, utils.cacheKey(key, 'layer', ldata.name), null, this);
+                var x = ldata.x ? ldata.x : ldata.offsetx;
+				var y = ldata.y ? ldata.y : ldata.offsety;
+                lyr = game.add.sprite(x, y, utils.cacheKey(key, 'layer', ldata.name), null, this);
 
                 lyr.visible = ldata.visible;
                 lyr.alpha = ldata.opacity;
